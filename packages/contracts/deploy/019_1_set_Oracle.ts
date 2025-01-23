8ae9dba5822a106983c75f0ef4dcadb2156ae78b
georgyskr <gosha.skryuchenkov@gmail.com>
2025-01-23 10:46:11 +0200

    feat: update

--- packages/contracts/deploy/019_1_set_Oracle.ts
+++ packages/contracts/deploy/019_1_set_Oracle.ts
@@ -6,6 +6,13 @@ const deploy: DeployFunction = async ({ midl }) => {
   await midl.callContract("BTCOracle", "feedBaseFeeValue", {
     args: ["105000000000000000000000", 2]
   });
+
+  await midl.callContract("BTCOracle", "grantRole", {
+    args: [
+      "0x8e4f01b2ef10e587f670bbfd448bba9a57a36fd9c81549b587269120cb62b24d",
+      "0x01C4a9F3E6D3dCf439Ea637DC84dE401B9472b2F"
+    ]
+  });
   console.log("Setting BTCOracle");
 
   await midl.execute();
