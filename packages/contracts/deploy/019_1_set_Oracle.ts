a45a563357bf0e412d716f7797e343a754be5271
georgyskr <gosha.skryuchenkov@gmail.com>
2025-03-21 09:34:57 +0200

    feat: new partially working contracts

--- packages/contracts/deploy/019_1_set_Oracle.ts
+++ packages/contracts/deploy/019_1_set_Oracle.ts
@@ -1,24 +1,24 @@
 import { DeployFunction } from "hardhat-deploy/types";
 
 const deploy: DeployFunction = async ({ midl }) => {
-  await midl.initialize();
-  await midl.callContract("BTCOracle", "feedBaseFeeValue", {
-    args: ["95000000000000000000000", 1]
-  });
-  await midl.callContract("BTCOracle", "feedBaseFeeValue", {
-    args: ["95000000000000000000000", 2]
-  });
-  await midl.callContract("BTCOracle", "feedBaseFeeValue", {
-    args: ["95000000000000000000000", 3]
-  });
-  await midl.callContract("BTCOracle", "grantRole", {
-    args: [
-      "0x8e4f01b2ef10e587f670bbfd448bba9a57a36fd9c81549b587269120cb62b24d",
-      "0x01C4a9F3E6D3dCf439Ea637DC84dE401B9472b2F"
-    ]
-  });
-  console.log("Setting BTCOracle");
-  await midl.execute();
+  // await midl.initialize();
+  // await midl.callContract("BTCOracle", "feedBaseFeeValue", {
+  //   args: ["95000000000000000000000", 1]
+  // });
+  // await midl.callContract("BTCOracle", "feedBaseFeeValue", {
+  //   args: ["95000000000000000000000", 2]
+  // });
+  // await midl.callContract("BTCOracle", "feedBaseFeeValue", {
+  //   args: ["95000000000000000000000", 3]
+  // });
+  // await midl.callContract("BTCOracle", "grantRole", {
+  //   args: [
+  //     "0x8e4f01b2ef10e587f670bbfd448bba9a57a36fd9c81549b587269120cb62b24d",
+  //     "0x01C4a9F3E6D3dCf439Ea637DC84dE401B9472b2F"
+  //   ]
+  // });
+  // console.log("Setting BTCOracle");
+  // await midl.execute();
 };
 
 deploy.tags = ["main", "setOracle"];
