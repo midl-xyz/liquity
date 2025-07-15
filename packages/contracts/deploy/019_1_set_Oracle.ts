86fd4bbecd3431ff8d510451a1d676d41a99ec97
georgyskr <gosha.skryuchenkov@gmail.com>
2025-07-15 22:15:21 +0300

    feat: update deployment

--- packages/contracts/deploy/019_1_set_Oracle.ts
+++ packages/contracts/deploy/019_1_set_Oracle.ts
@@ -2,21 +2,21 @@ import { DeployFunction } from "hardhat-deploy/types";
 
 const deploy: DeployFunction = async ({ midl }) => {
   await midl.initialize();
-  // await midl.callContract("BTCOracle", "feedBaseFeeValue", {
-  //   args: ["95000000000000000000000", 9]
-  // });
-  // await midl.callContract("BTCOracle", "feedBaseFeeValue", {
-  //   args: ["95000000000000000000000", 10]
-  // });
-  // await midl.callContract("BTCOracle", "feedBaseFeeValue", {
-  //   args: ["95000000000000000000000", 11]
-  // });
-  // await midl.callContract("BTCOracle", "grantRole", {
-  //   args: [
-  //     "0x8e4f01b2ef10e587f670bbfd448bba9a57a36fd9c81549b587269120cb62b24d",
-  //     "0x01C4a9F3E6D3dCf439Ea637DC84dE401B9472b2F"
-  //   ]
-  // });
+  await midl.callContract("BTCOracle", "feedBaseFeeValue", {
+    args: ["120000000000000000000000", 1]
+  });
+  await midl.callContract("BTCOracle", "feedBaseFeeValue", {
+    args: ["120000000000000000000000", 2]
+  });
+  await midl.callContract("BTCOracle", "feedBaseFeeValue", {
+    args: ["120000000000000000000000", 3]
+  });
+  await midl.callContract("BTCOracle", "grantRole", {
+    args: [
+      "0x8e4f01b2ef10e587f670bbfd448bba9a57a36fd9c81549b587269120cb62b24d",
+      "0x01C4a9F3E6D3dCf439Ea637DC84dE401B9472b2F"
+    ]
+  });
   console.log("Setting BTCOracle");
   await midl.execute();
 };
--- packages/dev-frontend/src/components/PriceManager.tsx
+++ packages/dev-frontend/src/components/PriceManager.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Card, Box, Heading, Flex, Button, Label, Input } from "theme-ui";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Card, Flex, Heading, Input, Label } from "theme-ui";
 
 import { Decimal, LiquityStoreState } from "@liquity/lib-base";
 import { useLiquitySelector } from "@liquity/lib-react";
